5fcefc37e43313b642c12d2c3b49b0b255a6fce1
Joshua Ling <joshling1919@gmail.com>
2019-07-30 09:19:54 -0700

    First commit

---
+++ docker-express-server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.port || 8000;
+
+app.get('/', (req, res) => {
+  res.sendFile('Hello World!');
+});
+
+app.listen(port, () => `Listening on ${port}`);
